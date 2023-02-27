016387e062bef6cf1531d5c55f728bee13e4cfd6
Pierre Anken <pierre.anken@proton.me>
2023-02-27 10:34:00 +0100

    Fix decode on string and small shadow of variables

--- wms5.py
+++ wms5.py
@@ -4,6 +4,7 @@ import hashlib
 import requests
 from urllib.parse import quote
 
+
 class Client():
     """
     A class used connect and interact with Wildix WMS5 API.
@@ -34,10 +35,10 @@ class Client():
     __encode_url
         encode API URL used on get or post query
     """
-    
+
     def __init__(self, config):
         """
-        Constructor with config parametres received in a dictionary
+        Constructor with config parameters received in a dictionary
         
         ...
         Config parameters
@@ -55,11 +56,11 @@ class Client():
         self.app_id = config["app_id"]
         self.app_name = config["app_name"]
         self.host = config["pbx_host"]
-    
+
     @property
     def secret(self):
         return self._secret
-    
+
     @secret.setter
     def secret(self, pbx_secret_key):
         if pbx_secret_key:
@@ -70,7 +71,7 @@ class Client():
     @property
     def app_id(self):
         return self._app_id
-    
+
     @app_id.setter
     def app_id(self, app_id):
         if app_id:
@@ -81,7 +82,7 @@ class Client():
     @property
     def app_name(self):
         return self._app_name
-    
+
     @app_name.setter
     def app_name(self, app_name):
         if app_name:
@@ -92,7 +93,7 @@ class Client():
     @property
     def pbx_host(self):
         return self._pbx_host
-    
+
     @pbx_host.setter
     def pbx_host(self, pbx_host):
         if pbx_host:
@@ -112,12 +113,12 @@ class Client():
 
         # Create bas request string. This part is common for all API requests
         request_string = "{}{}host:{};x-app-id:{};".format(
-                    self.__method,
-                    self.__url,
-                    self.host,
-                    self.app_id,
-                )
-        
+            self.__method,
+            self.__url,
+            self.host,
+            self.app_id,
+        )
+
         # Check options passed and generate custom request string
         for item in options_sorted.items():
             if isinstance(item[1], dict):
@@ -126,7 +127,7 @@ class Client():
                 request_string += "{}:{};".format(item[0], item[1])
 
         return request_string
-    
+
     def __generate_jwt(self, request_string):
         """
         Method to create Jason Web Tocken to authenticate with Wildix PBX
@@ -134,7 +135,7 @@ class Client():
 
         hash_string = hashlib.sha256(request_string.encode())
         timestamp = round(time.time())
-        expire = 1*60
+        expire = 1 * 60
 
         payload = {
             'iss': self.app_name,
@@ -143,8 +144,8 @@ class Client():
             'sign': {
                 'alg': 'sha256',
                 'headers': {
-                        '0': 'Host',
-                        '1': 'X-APP-ID',
+                    '0': 'Host',
+                    '1': 'X-APP-ID',
                 },
                 'hash': hash_string.hexdigest(),
             },
@@ -152,7 +153,7 @@ class Client():
 
         encoded_jwt = jwt.encode(payload, self.secret, algorithm='HS256')
         return encoded_jwt
-    
+
     def __encode_url(self, options):
         """
         Method to create the URL that will send to PBX.
@@ -172,7 +173,7 @@ class Client():
 
         return url
 
-    def query_get(self, url, options):
+    def query_get(self, url, options=None):
         """
         Method to query WMS API through GET method.
         """
@@ -180,18 +181,18 @@ class Client():
         self.__url = url
 
         # Call method to generate request string
-        request_string = self.__generate_request_string(options)
+        request_string = self.__generate_request_string(options or {})
 
         # Call method to generate JWT using request string
-        jwt = self.__generate_jwt(request_string).decode()
+        jwt_token = self.__generate_jwt(request_string)
 
         # Generate headers used on get request
-        head = {'Host': self.host, 'X-APP-ID': self.app_id, 'Authorization': 'Bearer {}'.format(jwt)}
+        head = {'Host': self.host, 'X-APP-ID': self.app_id, 'Authorization': 'Bearer {}'.format(jwt_token)}
 
         # Encode URL to be send to Wildix API
         url = self.__encode_url(options)
-        
-        return requests.get(url, headers = head)
+
+        return requests.get(url, headers=head)
 
     def query_post(self, url, options):
         """
@@ -204,11 +205,11 @@ class Client():
         request_string = self.__generate_request_string(options)
 
         # Call method to generate JWT using request string
-        jwt = self.__generate_jwt(request_string).decode()
+        jwt_token = self.__generate_jwt(request_string)
 
         # Generate headers used on get request
-        head = {'Host': self.host, 'X-APP-ID': self.app_id, 'Authorization': 'Bearer {}'.format(jwt)}
+        head = {'Host': self.host, 'X-APP-ID': self.app_id, 'Authorization': 'Bearer {}'.format(jwt_token)}
 
         post_url = "https://{}{}".format(self.host, self.__url)
-        
-        return requests.post(post_url, data=options, headers = head)
+
+        return requests.post(post_url, data=options, headers=head)
